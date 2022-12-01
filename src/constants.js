01e136e212ccbafa4d4a4d08f4ae14e368a8a46e
connorbelding <the_connn@hotmail.com>
2022-11-30 23:28:21 -0500

    Created constants file

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+const modes = {
+  solo: "solo",
+  multi: "multi",
+};
+
+const colors = {
+  blue: "rgb(32, 75, 192)",
+  black: "rgb(41, 41, 41)",
+  grey: "rgba(0, 0, 0, 0.5)",
+};
+
+export { modes, colors };
