7613f25bddd248935b8da171df920b2ff2dff4b1
connorbelding <the_connn@hotmail.com>
2022-12-01 00:31:54 -0500

    Added conditional rendering for screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./css/meyer-reset.css";
-import "./css/index.css"
-import ModeScreen from "./screens/mode-screen/mode-screen";
+import "./css/index.css";
+import { ModeScreen, NameScreen, GameScreen } from "./screens";
+import { screens } from "./constants";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [viewingScreen, setViewingScreen] = useState(screens.mode);
+
+  if (viewingScreen === screens.name) return <NameScreen />;
+  if (viewingScreen === screens.game) return <GameScreen />;
 
   return <ModeScreen />;
 }
