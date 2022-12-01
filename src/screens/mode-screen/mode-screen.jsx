be2e332baf9e1b29423249dd9e31a64051b825e5
connorbelding <the_connn@hotmail.com>
2022-12-01 00:03:42 -0500

    Removed all styling code

--- src/screens/mode-screen/mode-screen.jsx
+++ src/screens/mode-screen/mode-screen.jsx
@@ -1,12 +1,13 @@
-import { useState, useRef, useEffect } from "react";
-import { modes, colors } from "../../constants";
+import { useState } from "react";
+import { modes } from "../../constants";
+import { CustomRadioButton } from "../../components";
 import {
   FormScreenWrapper,
   Form,
   CenteredMsg,
+  RadioButtonsWrapper,
   SubmitButton,
 } from "../../shared-styles/shared-styles";
-import styled, { css } from "styled-components";
 
 export default function ModeScreen() {
   const [selectedMode, setSelectedMode] = useState(modes.solo);
@@ -25,14 +26,14 @@ export default function ModeScreen() {
       <Form onSubmit={handleSubmit}>
         <CenteredMsg fontSize="1.45rem">Select mode</CenteredMsg>
         <RadioButtonsWrapper>
-          <CustomButtonAccesible
+          <CustomRadioButton
             mode={modes.solo}
             groupName="mode"
             customButtonText="Solo (VS AI)"
             handleInputChange={handleInputChange}
             isChecked={selectedMode === modes.solo}
           />
-          <CustomButtonAccesible
+          <CustomRadioButton
             mode={modes.multi}
             groupName="mode"
             customButtonText="Multiplayer"
@@ -45,77 +46,3 @@ export default function ModeScreen() {
     </FormScreenWrapper>
   );
 }
-
-function CustomButtonAccesible({
-  groupName,
-  mode,
-  customButtonText,
-  handleInputChange,
-  isChecked,
-}) {
-  const inputRef = useRef();
-
-  function handleCustomButtonClick() {
-    inputRef.current.click();
-  }
-
-  return (
-    <RadioButtonWrapper>
-      <label htmlFor={mode}>{customButtonText}</label>
-      <input
-        ref={inputRef}
-        type="radio"
-        name={groupName}
-        id={mode}
-        value={mode}
-        checked={isChecked}
-        onChange={handleInputChange}
-      />
-      <RadioButton
-        type="button"
-        isSelected={isChecked}
-        onClick={handleCustomButtonClick}
-      >
-        {customButtonText}
-      </RadioButton>
-    </RadioButtonWrapper>
-  );
-}
-
-const RadioButtonsWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  gap: 1.2rem;
-  margin: 0.75rem 0 0 0;
-`;
-
-const RadioButtonWrapper = styled.div`
-  & label,
-  input {
-    height: 0;
-    width: 0;
-    position: absolute;
-    top: -99999px;
-    left: -99999px;
-  }
-`;
-
-const RadioButton = styled.button`
-  padding: 0.4rem 0.7rem;
-  border-radius: 8px;
-  user-select: none;
-  font-weight: 500;
-  font-size: 1.15rem;
-  ${(props) =>
-    props.isSelected
-      ? css`
-          background: ${colors.blue};
-          color: rgba(255, 255, 255, 1);
-          border: 2px solid ${colors.blue};
-        `
-      : css`
-          background: transparent;
-          color: ${colors.grey};
-          border: 2px solid ${colors.grey};
-        `}
-`;
