63cd0623540e0b5ebd20c12e007bb45f42f9b7b2
connorbelding <the_connn@hotmail.com>
2022-12-03 00:49:18 -0500

    Started implementing game logic

--- src/screens/game-screen/game-screen.jsx
+++ src/screens/game-screen/game-screen.jsx
@@ -1,3 +1,12 @@
-export default function GameScreen() {
+import { useState, useEffect } from "react";
+
+export default function GameScreen({ playMode, players, setViewingScreen }) {
+  const [playerOneScore, setPlayerOneScore] = useState(0);
+  const [playerTwoScore, setPlayerTwoScore] = useState(0);
+
+  useEffect(() => {
+    console.log({ playMode, players });
+  }, [playMode, players]);
+
   return <div>Game screen</div>;
 }
