b42fe2e75d82115b8e81d80fc9fc9d8169c924bd
connorbelding <the_connn@hotmail.com>
2022-12-05 00:25:01 -0500

    Started styling board

--- src/components/board/styles.js
+++ src/components/board/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { colors } from "../../constants";
 
 export const Wrapper = styled.div`
   height: 300px;
@@ -6,5 +7,18 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-rows: repeat(3, 1fr);
   grid-template-columns: repeat(3, 1fr);
-  border: 2px solid black;
+  border: 2px solid ${colors.lightGrey};
+  background: ${colors.lightGrey};
+  gap: 3px;
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.52);
+`;
+
+export const Tile = styled.button`
+  font-size: 42px;
+  font-weight: 500;
+  background: ${colors.white};
+  color: ${(props) => (props.mark === "x" ? "red" : "blue")};
+  &:disabled {
+    background: ${colors.white};
+  }
 `;
