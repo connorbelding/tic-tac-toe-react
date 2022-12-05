5692bf3d104a69edab9262ceda70b68ba2ccb1b3
connorbelding <the_connn@hotmail.com>
2022-12-05 00:22:18 -0500

    Added light grey color to colors

--- src/constants.js
+++ src/constants.js
@@ -1,26 +1,28 @@
-const modes = {
+export const modes = {
   solo: "solo",
   multi: "multi",
 };
 
-const colors = {
+export const colors = {
   blue: "rgb(32, 75, 192)",
   black: "rgb(41, 41, 41)",
   grey: "rgba(0, 0, 0, 0.5)",
+  lightGrey: "rgba(0, 0, 0, 0.25)",
+  white: "rgb(255, 255, 255)",
 };
 
-const screens = {
+export const screens = {
   mode: "mode screen",
   name: "name screen",
   game: "game screen",
 };
 
-const marks = {
+export const marks = {
   x: "x",
   o: "o",
 };
 
-const potentialWins = [
+export const potentialWins = [
   "0,0/0,1/0,2", //top horizontal
   "0,0/1,0/2,0", //left vertical
   "0,0/1,1/2,2", //right down diagonal
@@ -30,5 +32,3 @@ const potentialWins = [
   "0,2/1,2/2,2", //right vertical
   "2,0/2,1/2,2", //bottom horizontal
 ];
-
-export { modes, colors, screens, marks, potentialWins };
