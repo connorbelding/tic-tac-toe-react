d07a6c40ff8824b3bb38c84224a8d7d10b820c3d
connorbelding <the_connn@hotmail.com>
2022-12-05 00:26:42 -0500

    Started styling game screen

--- src/screens/game-screen/game-screen.jsx
+++ src/screens/game-screen/game-screen.jsx
@@ -1,9 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Board } from "../../components";
+import { Wrapper, ScoresWrapper, BoardWrapper, QuitWrapper } from "./styles";
+import { screens } from "../../constants";
 
 export default function GameScreen({ playMode, players, setViewingScreen }) {
   const [playerOneScore, setPlayerOneScore] = useState(0);
   const [playerTwoScore, setPlayerTwoScore] = useState(0);
 
-  return <Board />;
+  useEffect(() => {
+    console.log({ players, playMode });
+  }, [players, playMode]);
+
+  return (
+    <Wrapper>
+      <ScoresWrapper>
+        <div>
+          <div>
+            {players.playerOne.name} ({players.playerOne.mark.toUpperCase()})
+          </div>
+          <div>
+            {playerOneScore}
+          </div>
+        </div>
+        <div>
+          <div>{players.playerTwo.name} ({players.playerTwo.mark.toUpperCase()})</div>
+          <div>{playerTwoScore}</div>
+        </div>
+      </ScoresWrapper>
+      <BoardWrapper>
+        <Board
+          setPlayerOneScore={setPlayerOneScore}
+          setPlayerTwoScore={setPlayerTwoScore}
+          players={players}
+          playMode={playMode}
+        />
+      </BoardWrapper>
+      <QuitWrapper>
+        <button
+          onClick={() => {
+            setViewingScreen(screens.mode);
+          }}
+        >
+          Quit
+        </button>
+      </QuitWrapper>
+    </Wrapper>
+  );
 }
