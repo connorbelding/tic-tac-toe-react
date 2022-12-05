65aee9f011271fe53778356a65f9e62a609ee9cd
connorbelding <the_connn@hotmail.com>
2022-12-05 00:27:34 -0500

    Created game screen styles

---
+++ src/screens/game-screen/styles.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  max-width: 1600px;
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  padding: 1rem;
+`;
+
+export const ScoresWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const BoardWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const QuitWrapper = styled.div``;
