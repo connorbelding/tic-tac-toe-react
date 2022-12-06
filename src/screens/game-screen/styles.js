e302d90bed6e8f4d93d9aeeb6d0f8ef1683d38d4
connorbelding <the_connn@hotmail.com>
2022-12-06 17:16:32 -0500

    Created ReplayWindow component

--- src/screens/game-screen/styles.js
+++ src/screens/game-screen/styles.js
@@ -7,6 +7,7 @@ export const Wrapper = styled.div`
   display: grid;
   grid-template-rows: auto 1fr auto;
   padding: 1rem;
+  position: relative;
 `;
 
 export const ScoresWrapper = styled.div`
@@ -21,3 +22,15 @@ export const BoardWrapper = styled.div`
 `;
 
 export const QuitWrapper = styled.div``;
+
+export const ReplayWindow = styled.form`
+  position: absolute;
+  top: 2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  background: #fff;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  padding: 1rem;
+`;
